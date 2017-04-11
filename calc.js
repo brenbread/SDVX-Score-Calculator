74805131f9aeeb63865716d8bc0f16e431dc6261
brenbread <santos.brenj@gmail.com>
2017-04-10 18:47:10 -0700

    Updated HTML and added grade to calculation
    Grade info from here:
    
    http://bemaniwiki.com/index.php?SOUND%20VOLTEX%20IV%20HEAVENLY%20HAVEN#h57277c2

--- calc.js
+++ calc.js
@@ -16,5 +16,40 @@ function scoreCalc() {
   else {
     totScore = (criticalVal*critical)+(nearVal*near);
   }
+  var grade ="";
+  if(totScore>=9900000) {
+    grade="S";
+  }
+  else if(totScore>=9800000) {
+    grade="AAA+";
+  }
+  else if(totScore>=9700000) {
+    grade="AAA";
+  }
+  else if(totScore>=9500000) {
+    grade="AA+";
+  }
+  else if(totScore>=9300000) {
+    grade = "AA";
+  }
+  else if(totScore>=9000000) {
+    grade="A+";
+  }
+  else if(totScore>=8700000) {
+    grade = "A";
+  }
+  else if(totScore>=7500000) {
+    grade = "B";
+  }
+  else if (totScore>=6500000) {
+    grade = "C";
+  }
+  else if (totScore<6500000 && totScore>0) {
+    grade="D";
+  }
+  else if (totScore==0){
+    grade="Did you even play?";
+  }
   document.getElementById("score").innerHTML = Math.floor(totScore);
+  document.getElementById("grade").innerHTML = grade;
 }
